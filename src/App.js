aaff117812e1ce00ab8e887e051d7a35cb56be19
Callum Fletcher <callumafletcher@gmail.com>
2021-04-03 15:28:04 -0700

    added responsive grid layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { useEffect } from "react";
+import { useMediaQuery } from "@material-ui/core";
 import "./App.css";
 
 function App() {
   return (
     <div className="root">
-      <div className="content">
+      <div className="body">
         <div className="title-container">
           <h1 className="title">Callum</h1>
           {/* get rid of padding on smaller screen */}
-          <h1 className="title" style={{ paddingLeft: 20 }}>
-            Fletcher
-          </h1>
+          <h1 className="title">Fletcher</h1>
         </div>
+        <div className="nav-menu">lkjlkjlk</div>
+        <div className="content">lkjlkjlk</div>
+        <div className="bottom"></div>
       </div>
-      <div className="graph" />
-      <div className="vert-graph" />
+      <div className="bg" />
+      <div className="vert-bg" />
     </div>
   );
 }
---
+++ src/components/NavMenu.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./NavMenu.css";
+
+function NavMenu() {
+  return <div></div>;
+}
+
+export default NavMenu;
