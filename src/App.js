da572694eafd0ef63cf2a6bb196a3049cc216b06
Callum Fletcher <callumafletcher@gmail.com>
2021-04-03 16:36:42 -0700

    nav menu ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React, { useEffect } from "react";
 import { useMediaQuery } from "@material-ui/core";
 import "./App.css";
+import NavMenu from "./components/NavMenu";
 
 function App() {
   return (
     <div className="root">
+      <div className="bg" />
+      <div className="vert-bg" />
       <div className="body">
         <div className="title-container">
           <h1 className="title">Callum</h1>
           {/* get rid of padding on smaller screen */}
           <h1 className="title">Fletcher</h1>
         </div>
-        <div className="nav-menu">lkjlkjlk</div>
+        <div className="nav-menu">
+          <NavMenu />
+        </div>
         <div className="content">lkjlkjlk</div>
         <div className="bottom"></div>
       </div>
-      <div className="bg" />
-      <div className="vert-bg" />
     </div>
   );
 }
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavMenu.css";
 
 function NavMenu() {
-  return <div></div>;
+  return (
+    <>
+      <div className="active-page"></div>
+      {/* pointerEvents: "none" for active page */}
+      <h3 className="page">About</h3>
+      <h3 className="page">Portfolio</h3>
+      <h3 className="page">Contact</h3>
+    </>
+  );
 }
 
 export default NavMenu;
