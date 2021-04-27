65848f8c3546a96ce9dc140381d7be9e2dcf4784
Callum Fletcher <callumafletcher@gmail.com>
2021-04-27 02:22:02 -0700

    portfolio items and transition

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,44 +1,122 @@
-import { Fade, Grid, Tab, Tabs } from "@material-ui/core";
-import React, { useState } from "react";
-
+import { Fade, Grid, IconButton, Popover, Tab, Tabs } from "@material-ui/core";
+import React, { useRef, useState } from "react";
 import "./MainContent.css";
+import { FilterIcon } from "./Icons";
+import FilterPopover from "./FilterPopover";
+import PortfolioPreviewItem from "./PortfolioPreviewItem";
+import PortfolioItem from "./PortfolioItem";
 
 function Portfolio({ portfolioRef, portfolioInView }) {
-  const [value, setValue] = useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const ref = useRef();
+  //TODO: media query to lower number of items shown on small screen
+  const [selection, setSelection] = useState(null);
+  const [selectionPosition, setSelectionPosition] = useState(null);
+  const defaultSelection = {
+    type: {
+      "Work Experience": false,
+      "Personal Projects": false,
+      "Hackathon Projects": false,
+    },
+    language: {
+      JavaScript: false,
+      Python: false,
+      "HTML/CSS": false,
+      "C#": false,
+    },
+    framework: {
+      React: false,
+      Express: false,
+      "Node.js": false,
+      ".Net Core": false,
+      MongoDB: false,
+      Firebase: false,
+    },
   };
-
+  const [filter, setFilter] = useState({
+    page: 0,
+    ...defaultSelection,
+  });
+  const handleChange = (event, newValue) => {
+    setFilter((prev) => ({ ...defaultSelection, page: newValue }));
+  };
+  console.log(ref?.current?.getBoundingClientRect());
   return (
     <Fade in={portfolioInView} timeout={1000}>
       <div className="section" ref={portfolioRef} id="portfolio">
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          style={{
-            color: "#ffb7f3",
-            marginBottom: 10,
-          }}
-          TabIndicatorProps={{ style: { background: "#ffb7f3" } }}
-        >
-          <Tab label="Featured" classes={{ root: { background: "blue" } }} />
-          <Tab label="Projects" />
-          <Tab label="Work Experience" />
-        </Tabs>
-        <Grid container spacing={1} style={{ height: "100%" }}>
-          <Grid item xs={12} sm={6}>
-            <div className="portfolio-item"></div>
+        <div className="portfolio-nav">
+          <Tabs
+            variant="fullWidth"
+            value={filter.page}
+            onChange={handleChange}
+            className="portfolio-tabs"
+            style={{
+              marginBottom: 10,
+            }}
+            TabIndicatorProps={{ style: { background: "#ffb7f3" } }}
+          >
+            <Tab label="Featured" style={{ fontWeight: 400, fontSize: 18 }} />
+            <Tab label="Projects" style={{ fontWeight: 400, fontSize: 18 }} />
+            <Tab
+              label="Work Experience"
+              style={{ fontWeight: 400, fontSize: 18 }}
+            />
+          </Tabs>
+          <IconButton
+            onClick={(e) => setAnchorEl(e.currentTarget)}
+            style={{ fontWeight: 400, fontSize: 18, color: "#ffb7f3" }}
+          >
+            <FilterIcon />
+          </IconButton>
+          <Popover
+            open={Boolean(anchorEl)}
+            anchorEl={anchorEl}
+            onClose={() => setAnchorEl(null)}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+          >
+            <FilterPopover filter={filter} setFilter={setFilter} />
+          </Popover>
+        </div>
+        {selectionPosition ? (
+          <PortfolioItem
+            initialPosition={{
+              position: "fixed",
+              width: selectionPosition.width,
+              height: selectionPosition.height,
+              top: selectionPosition.top,
+              bottom: selectionPosition.bottom,
+              left: selectionPosition.left,
+              right: selectionPosition.right,
+            }}
+            finalPosition={{
+              width: ref?.current?.getBoundingClientRect().width,
+              height: ref?.current?.getBoundingClientRect().height,
+              top: ref?.current?.getBoundingClientRect().top,
+              bottom: ref?.current?.getBoundingClientRect().bottom,
+              left: ref?.current?.getBoundingClientRect().left,
+              right: ref?.current?.getBoundingClientRect().right,
+            }}
+          />
+        ) : (
+          <Grid container spacing={1} style={{ height: "100%" }} ref={ref}>
+            {[...Array(6)].map((element, index) => (
+              <PortfolioPreviewItem
+                key={index}
+                name={index}
+                selection={selection}
+                setSelection={setSelection}
+                setSelectionPosition={setSelectionPosition}
+              />
+            ))}
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <div className="portfolio-item"></div>
-          </Grid>{" "}
-          <Grid item xs={12} sm={6}>
-            <div className="portfolio-item"></div>
-          </Grid>{" "}
-          <Grid item xs={12} sm={6}>
-            <div className="portfolio-item"></div>
-          </Grid>{" "}
-        </Grid>
+        )}
       </div>
     </Fade>
   );
