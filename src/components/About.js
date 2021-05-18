9131bb9fad8ae6b5d01e41adeda0b23cfb1f3cd2
Callum Fletcher <callumafletcher@gmail.com>
2021-05-17 21:42:09 -0400

    updating info

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,8 @@ function About({ aboutRef, aboutInView }) {
       <div className="section" id="about" ref={aboutRef}>
         <p className="text">
           Hi I'm Callum. I am a web developer and Engineering student at the
-          University of Waterloo.
+          University of Waterloo. I am currently looking for a 4 month
+          internship for Fall 2021.
         </p>
       </div>
     </Fade>
